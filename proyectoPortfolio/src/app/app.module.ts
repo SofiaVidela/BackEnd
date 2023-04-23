d70116b45a19cfb74a49d779d5d421dbca0dfa02
SofiaVidela <71666710+SofiaVidela@users.noreply.github.com>
2023-04-23 20:39:21 -0300

    Avance-Secciones: Experiencia y Educación
    Incorporación de bootstrap al proyecto de angular
    nuevos componentes Educación y Experiencia
    (con uso de acordeón clase de bootstrap )
    Cambio de paleta de color

--- proyectoPortfolio/src/app/app.module.ts
+++ proyectoPortfolio/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
