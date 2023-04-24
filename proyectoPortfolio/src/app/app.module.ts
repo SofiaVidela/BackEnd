c7742290ec592fa1aa5835a60ac8709bd0a7cde5
SofiaVidela <71666710+SofiaVidela@users.noreply.github.com>
2023-04-24 18:05:39 -0300

    eliminacion del  componente LogoAP

--- proyectoPortfolio/src/app/app.module.ts
+++ proyectoPortfolio/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -19,7 +18,6 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
