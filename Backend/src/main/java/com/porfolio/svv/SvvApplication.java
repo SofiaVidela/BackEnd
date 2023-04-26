5d011c863a056e47ebccc1ec7d581bec32a5ac16
SofiaVidela <71666710+SofiaVidela@users.noreply.github.com>
2023-04-26 02:31:05 -0300

    conexion a base de datos correcta
    backendsvv nombre de la base de datos (puerto 3307)

--- Backend/src/main/java/com/porfolio/svv/SvvApplication.java
+++ Backend/src/main/java/com/porfolio/svv/SvvApplication.java
@@ -1,4 +1,4 @@
-package com.portfolio.svv;
+package com.porfolio.svv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/src/test/java/com/porfolio/svv/SvvApplicationTests.java
+++ Backend/src/test/java/com/porfolio/svv/SvvApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolio.svv;
+package com.porfolio.svv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
