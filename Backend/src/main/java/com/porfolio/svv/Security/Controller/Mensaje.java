99897915c2b363f691dae3328abe754f8535193b
SofiaVidela <71666710+SofiaVidela@users.noreply.github.com>
2023-04-29 02:13:11 -0300

    Back del Login
    Compila sin errores y conecta
    Faltan Pruebas en Postman

---
+++ Backend/src/main/java/com/porfolio/svv/Security/Controller/Mensaje.java
@@ -0,0 +1,26 @@
+
+package com.porfolio.svv.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    //Constructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    //Getter y Setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
---
+++ Backend/src/main/java/com/porfolio/svv/Security/Enums/RolNombre.java
@@ -0,0 +1,7 @@
+
+package com.porfolio.svv.Security.Enums;
+
+
+public enum RolNombre {
+    ROLE_ADMIN,ROLE_USER
+}
